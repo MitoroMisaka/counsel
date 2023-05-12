70e10c675ffd38de3193f68ca16c7f3cd68c02a1
Arkidian <2366881210@qq.com>
2023-05-12 13:28:07 +0800

    添加 Mabatis Plus 以及对应的简单测试

--- src/main/java/com/ecnu/rai/counsel/entity/User.java
+++ src/main/java/com/ecnu/rai/counsel/entity/User.java
@@ -1,6 +1,9 @@
 package com.ecnu.rai.counsel.entity;
 
 import javax.validation.constraints.NotNull;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
@@ -14,13 +17,14 @@ import javax.validation.constraints.Size;
 @AllArgsConstructor
 @ApiModel(description = "用户信息")
 public class User {
+    @TableId(type = IdType.AUTO)
     @ApiModelProperty(value = "用户ID")
     private Long id;
 
     @ApiModelProperty(value = "用户名", example = "john")
     @NotNull(message = "用户名不能为空")
     @Size(min = 3, max = 20, message = "用户名长度必须在3到20个字符之间")
-    private String userName;
+    private String username;
 
     @ApiModelProperty(value = "密码", example = "123456")
     @NotNull(message = "密码不能为空")
@@ -28,6 +32,6 @@ public class User {
     private String password;
 
     @ApiModelProperty(value = "角色", example = "admin")
-    private String character;
+    private String type;
 }
 
---
+++ src/main/java/com/ecnu/rai/counsel/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.ecnu.rai.counsel.mapper;
+
+import com.ecnu.rai.counsel.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface UserMapper extends BaseMapper<User> {
+}
