9c529718e813dff1b60ff3aa32c0ed52e5e44262
wangjustar <wangjustar@gmail.com>
2023-05-16 10:01:20 +0800

    登录

--- src/main/java/com/ecnu/rai/counsel/controller/WXController.java
+++ src/main/java/com/ecnu/rai/counsel/controller/WXController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.Objects;
 
 @Slf4j
 @Controller
@@ -29,20 +30,59 @@ public class WXController {
     @RequestMapping("/wx/login")
     public String login(HttpServletRequest request) throws IOException {
         String code = request.getParameter("code");
-        String state = request.getParameter("state");
+//        String state = request.getParameter("state");
+//        if (code == null){
+//            log.error("用户取消登录");
+//        }
+//        log.info("code = {}", code);
+//        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+//        String url = "https://api.weixin.qq.com/sns/oauth2/access_token" +
+//                "?appid=" + wxConfig.getAppId() +
+//                "&secret=" + wxConfig.getAppSecret() +
+//                "&code=" + code +
+//                "&grant_type=authorization_code";
+//        HttpGet httpGet = new HttpGet(url);
+//        CloseableHttpResponse response = httpClient.execute(httpGet);
+//        HttpEntity entity = response.getEntity();
+//        String body = CommonUtil.getBody(entity.getContent());
+//        log.info(body);
+//        response.close();
+//
+//        JSONObject bodyJson = JSON.parseObject(body);
+//        // 这里已经获取了用户的部分信息，可以在数据库中查询，如果已经记录过了，就没有必要进入后面的步骤了
+//        String accessToken = bodyJson.getString("access_token");
+//        String openId = bodyJson.getString("openid");
+//        String refreshToken = bodyJson.getString("refresh_token");
+//        url = "https://api.weixin.qq.com/sns/oauth2/refresh_token" +
+//                "appid="+wxConfig.getAppId()+"grant_type=refresh_token&refresh_token="
+//        +refreshToken;
+//        httpGet = new HttpGet(url);
+//        response = httpClient.execute(httpGet);
+//        body = CommonUtil.getBody(response.getEntity().getContent());
+//        bodyJson = JSON.parseObject(body);
+//        accessToken = bodyJson.getString("access_token");
+//        url = "https://api.weixin.qq.com/sns/userinfo" +
+//                "?access_token=" + accessToken +
+//                "&openid=" + openId + "&lang=zh_CN";
+//        httpGet = new HttpGet(url);
+//        response = httpClient.execute(httpGet);
+//        body = CommonUtil.getBody(response.getEntity().getContent());
+//        bodyJson = JSON.parseObject(body);
+//        // 获取了用户信息后进行存储
+//        log.info("the info of user is {}", bodyJson);
+//        response.close();
+//        // 登录成功，设置session
+//        request.getSession().setAttribute("unionId", bodyJson.getString("union_id"));
+//        return "登录成功";
         if (code == null){
             log.error("用户取消登录");
         }
-        log.info("code = {}", code);
+        String url = "https://api.weixin.qq.com/sns/jscode2session?appid="
+                 +wxConfig.getAppId() +"&secret="+wxConfig.getAppSecret()
+                 +"&js_code="+code+"&grant_type=authorization_code";
         CloseableHttpClient httpClient = HttpClientBuilder.create().build();
-        String url = "https://api.weixin.qq.com/sns/oauth2/access_token" +
-                "?appid=" + wxConfig.getAppId() +
-                "&secret=" + wxConfig.getAppSecret() +
-                "&code=" + code +
-                "&grant_type=authorization_code";
         HttpGet httpGet = new HttpGet(url);
         CloseableHttpResponse response = httpClient.execute(httpGet);
-
         HttpEntity entity = response.getEntity();
         String body = CommonUtil.getBody(entity.getContent());
         log.info(body);
@@ -50,12 +90,23 @@ public class WXController {
 
         JSONObject bodyJson = JSON.parseObject(body);
         // 这里已经获取了用户的部分信息，可以在数据库中查询，如果已经记录过了，就没有必要进入后面的步骤了
+        String openId = bodyJson.getString("openid");
+        url ="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid="
+                +wxConfig.getAppId() +"&secret="+wxConfig.getAppSecret();
+//        String accessToken = bodyJson.getString("access_token");
+//        String refreshToken = bodyJson.getString("refresh_token");
+//        url = "https://api.weixin.qq.com/sns/oauth2/refresh_token" +
+//                "appid="+wxConfig.getAppId()+"grant_type=refresh_token&refresh_token="
+//                +refreshToken;
+        httpGet = new HttpGet(url);
+        response = httpClient.execute(httpGet);
+        body = CommonUtil.getBody(response.getEntity().getContent());
+        bodyJson = JSON.parseObject(body);
         String accessToken = bodyJson.getString("access_token");
-        String openId = bodyJson.getString("openId");
-
+        log.info("access_token:{}",accessToken);
         url = "https://api.weixin.qq.com/sns/userinfo" +
                 "?access_token=" + accessToken +
-                "&openid=" + openId;
+                "&openid=" + openId + "&lang=zh_CN";
         httpGet = new HttpGet(url);
         response = httpClient.execute(httpGet);
         body = CommonUtil.getBody(response.getEntity().getContent());
