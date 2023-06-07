9b9d85c747021e38a9d666afc9e7206ae5421c28
wangjustar <wangjustar@gmail.com>
2023-06-07 13:26:40 +0800

    sms短信

--- src/main/java/com/ecnu/rai/counsel/controller/AccountController.java
+++ src/main/java/com/ecnu/rai/counsel/controller/AccountController.java
@@ -7,7 +7,6 @@ import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.ecnu.rai.counsel.common.Result;
-import com.ecnu.rai.counsel.dao.NormalRequest;
 import com.ecnu.rai.counsel.entity.*;
 import com.ecnu.rai.counsel.mapper.*;
 import com.ecnu.rai.counsel.response.GetUserResponse;
