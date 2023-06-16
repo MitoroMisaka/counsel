fd2cdd6ff8aefb87352769d57fc003f22df4fb05
wangjustar <wangjustar@gmail.com>
2023-06-16 08:30:27 +0800

    1

--- src/main/java/com/ecnu/rai/counsel/service/impl/WXServiceImpl.java
+++ src/main/java/com/ecnu/rai/counsel/service/impl/WXServiceImpl.java
@@ -1,7 +1,10 @@
 package com.ecnu.rai.counsel.service.impl;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.ecnu.rai.counsel.common.Page;
+import com.ecnu.rai.counsel.common.Result;
 import com.ecnu.rai.counsel.entity.User;
 import com.ecnu.rai.counsel.entity.Visitor;
 import com.ecnu.rai.counsel.mapper.MyMapper;
@@ -9,11 +12,14 @@ import com.ecnu.rai.counsel.mapper.UserMapper;
 import com.ecnu.rai.counsel.mapper.VisitorMapper;
 import com.ecnu.rai.counsel.service.AccountService;
 import com.ecnu.rai.counsel.service.WXService;
+import com.ecnu.rai.counsel.util.TokenUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
 
 @Service
@@ -56,5 +62,21 @@ public class WXServiceImpl implements WXService {
         if(user.getState() == 1)return true;
         else return false;
     }
+
+    @Override
+    public User finduserByopenid(String token) {
+        DecodedJWT jwt = JWT.decode(token);
+        String openid = jwt.getClaim("openid").asString();
+        Long id = visitorMapper.findIdbyopenid(openid);
+        return userMapper.findById(id);
+    }
+
+//    @Override
+//    public List<HashMap<String, Object>> findAvaliableCounselorInfo(LocalDateTime localDateTime) {
+//        List l = visitorMapper.findAvailableCounselor(localDateTime);
+//        for(Object i:l){
+//
+//        }
+//    }
 }
 
